0975ec6f2875ce3568eba2633fdb477c33a08330
dixyes <dixyes@gmail.com>
2022-10-20 16:18:53 +0800

    Add test methods

--- Makefile
+++ Makefile
@@ -1,11 +1,23 @@
-CFLAGS?=-std=c89 -ansi -pedantic -O4 -Wall -fPIC 
+CFLAGS ?= -std=gnu99 -O4 -Wall -fPIC
+EXAMPLE_BINARIES = examples/callback_parser examples/pause_parser
 
 default: multipart_parser.o
 
 multipart_parser.o: multipart_parser.c multipart_parser.h
 
 solib: multipart_parser.o
-	$(CC) -shared -Wl,-soname,libmultipart.so -o libmultipart.so multipart_parser.o
+	$(CC) $(CFLAGS) -shared -Wl,-soname,libmultipart.so -o libmultipart.so multipart_parser.o
+
+examples: $(EXAMPLE_BINARIES)
+
+examples/% : examples/%.c multipart_parser.o
+	$(CC) $(CFLAGS) -I. $< multipart_parser.o -o examples/$*
+
+tests: $(EXAMPLE_BINARIES)
+	@cat test/hello_world.data | examples/callback_parser >&-
+	@cat test/hello_world.data | examples/pause_parser >&-
+	@echo done
 
 clean:
-	rm -f *.o *.so
+	-rm -f *.o *.so
+	-rm $(EXAMPLE_BINARIES)
---
+++ examples/pause_parser.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include "multipart_parser.h"
+
+#define BOUNDARY_STR "hello world"
+
+// all the callbacks is paused
+// data callbacks
+long on_header_field(multipart_parser *p, const char *at, size_t length) {
+    printf("[HEADER_FIELD] \"%.*s\"\n", (int)length, at);
+    return MPPE_PAUSED;
+}
+
+long on_header_value(multipart_parser *p, const char *at, size_t length) {
+    printf("[HEADER_VALUE] \"%.*s\"\n", (int)length, at);
+    return MPPE_PAUSED;
+}
+
+long on_part_data(multipart_parser *p, const char *at, size_t length) {
+    printf("[PART_DATA] \"%.*s\"\n", (int)length, at);
+    return MPPE_PAUSED;
+}
+
+// notify callbacks
+long on_part_data_begin(multipart_parser *p) {
+    printf("[PART_DATA_BEGIN]\n");
+    return MPPE_PAUSED;
+}
+
+long on_headers_complete(multipart_parser *p) {
+    printf("[HEADERS_COMPLETE]\n");
+    return MPPE_PAUSED;
+}
+
+long on_part_data_end(multipart_parser *p) {
+    printf("[PART_DATA_END]\n");
+    return MPPE_PAUSED;
+}
+
+long on_body_end(multipart_parser *p) {
+    printf("[BODY_END]\n");
+    return MPPE_PAUSED;
+}
+
+int main(int argc, char **argv) {
+    const char *boundary = BOUNDARY_STR;
+    if (argc > 1) {
+        boundary = argv[1];
+    }
+    size_t boundary_len = strnlen(boundary, 70);
+
+    multipart_parser_settings settings;
+    settings.on_header_field = (multipart_data_cb)on_header_field;
+    settings.on_header_value = (multipart_data_cb)on_header_value;
+    settings.on_part_data = (multipart_data_cb)on_part_data;
+    settings.on_part_data_begin = (multipart_notify_cb)on_part_data_begin;
+    settings.on_headers_complete = (multipart_notify_cb)on_headers_complete;
+    settings.on_part_data_end = (multipart_notify_cb)on_part_data_end;
+    settings.on_body_end = (multipart_notify_cb)on_body_end;
+
+    multipart_parser parser;
+
+    multipart_parser_init((multipart_parser *)&parser, boundary, boundary_len, &settings);
+
+    char buffer[8192] = { '\0' };
+    size_t buffer_len = 0;
+    size_t buffer_parsed;
+    while ((buffer_len = read(0, buffer, sizeof(buffer))) > 0) {
+        buffer_parsed = 0;
+        size_t parsed;
+        while ((parsed = multipart_parser_execute((multipart_parser *)&parser, &buffer[buffer_parsed], buffer_len - buffer_parsed)) > 0) {
+            if (parsed == MPPE_ERROR) {
+                // parse failed
+                multipart_parser_error_msg((multipart_parser *)&parser, buffer, sizeof(buffer) - 1);
+                fprintf(stderr, "failed parse %s\n", buffer);
+                return 1;
+            }
+
+            // since it paused,
+            // you may return here when work with llhttp
+            buffer_parsed += parsed;
+        }
+
+        // move remaining buffer to head
+        // if invoke in llhttp, you may simply reuse the buffer
+        memmove(buffer, &buffer[buffer_parsed], buffer_len - buffer_parsed);
+        memset(&buffer[buffer_len - buffer_parsed], 0, buffer_parsed);
+    }
+
+    return 0;
+}
